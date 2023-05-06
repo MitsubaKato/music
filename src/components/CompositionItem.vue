01d56e7410341cd9af1b1a458252b4b220266691
MitsubaKato <alexreals78@gmail.com>
2023-05-06 20:48:30 +0300

    Убрано расширение mp3

--- src/components/CompositionItem.vue
+++ src/components/CompositionItem.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="border border-gray-200 p-3 mb-4 rounded">
     <div v-show="!showForm">
-      <h4 class="inline-block text-2xl font-bold">{{ song.modified_name }}</h4>
+      <h4 class="inline-block text-2xl font-bold">{{ song.modified_name.split(".")[0] }}</h4>
       <button
         class="ml-1 py-1 px-2 text-sm rounded text-white bg-red-600 float-right"
         @click.prevent="deleteSong"
